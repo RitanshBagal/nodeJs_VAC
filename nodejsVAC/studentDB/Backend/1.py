3a5e4f1d39664ee71913433ef1707a0cda418fc9
Ritansh Bagal <104382534+RitanshBagal@users.noreply.github.com>
2024-02-23 22:51:37 +0530

    Add files via upload

---
+++ nodejsVAC/studentDB/Backend/1.py
@@ -0,0 +1,8 @@
+import time
+a = 1
+b = 2 
+c = 5
+print(a)
+print(b)
+time.sleep(2)
+print(c)
---
+++ nodejsVAC/studentDB/Backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const port = 3000;
+
+const {connection} = require("./db/db_config");
+app.get('/getTopper', (req,res)=>{
+    connection.query(`SELECT * FROM student_marks WHERE marks = (SELECT MAX(marks) FROM student_marks)`, (err,results)=>{
+        if(err){
+            console.log(`Error in query!`);
+            res.send("Error in DB query");
+        }
+        else{
+            console.log(results);
+            res.send(results);
+        }
+    });
+});
+
+
+app.get('/getFailedList', (req, res) => {
+    // let userInput = req.query.term;
+    connection.query(`SELECT * FROM student_marks WHERE marks < 50`,
+    (err, results)=>{
+        if(err){
+            console.log(`Error in query!`);
+            res.send("Error in DB query")
+        }
+        else{
+            console.log(results);
+            res.send(results);
+        }
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
